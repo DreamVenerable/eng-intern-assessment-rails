8b4bdaad48f000692fdf693d8801b801cbc75995
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2024-01-11 09:49:37 +0200

    added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  # Searches for records with titles or content containing the specified query,
+  # using SQL 'LIKE' for partial matching and ensuring sanitized input to prevent SQL injection.
   def self.search(query)
     where("title LIKE :query OR content LIKE :query", query: "%#{sanitize_sql_like(query)}%")
   end
